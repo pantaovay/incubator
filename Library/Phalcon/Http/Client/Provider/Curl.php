091631dc268253549324ec851799c8eeec0ee3e7
Tao Pan <pantaovay@gmail.com>
2016-10-18 11:14:13 +0800

    Bug Fix: curl response content contains header

--- Library/Phalcon/Http/Client/Provider/Curl.php
+++ Library/Phalcon/Http/Client/Provider/Curl.php
@@ -129,9 +129,9 @@ class Curl extends Request
         $response->header->parse($this->responseHeader);
 
         if ($fullResponse) {
-            $response->body = $this->responseHeader . $content;
-        } else {
             $response->body = $content;
+        } else {
+            $response->body = substr($content, strlen($this->responseHeader));
         }
 
         return $response;
